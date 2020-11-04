1e1ec05177834104a92d42c859cbd6c20f22926c
Tomasz Wodzynski <t910755@gmail.com>
2020-11-04 16:55:48 +0100

    feat(initial): initial commit for service

---
+++ Service/IRegisterService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Service
+{
+    [ServiceContract]
+    public interface IRegisterService
+    {
+        [OperationContract]
+        void AddPatient(Patient.Patient patient);
+
+        [OperationContract]
+        IEnumerable<Patient.Patient> GetPatients();
+
+        [OperationContract]
+        void AddContact(Patient.PatientContact contact);
+    }
+}
---
+++ Service/RegisterService.svc.cs
@@ -0,0 +1,35 @@
+﻿using Patient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Service
+{
+
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class RegisterService : IRegisterService
+    {
+        private List<Patient.Patient> patients = new List<Patient.Patient>();
+        private List<Patient.PatientContact> patientContacts = new List<PatientContact>();
+
+        public void AddPatient(Patient.Patient patient)
+        {
+            patients.Add(patient);
+        }
+
+        public void AddContact(PatientContact contact)
+        {
+            patientContacts.Add(contact);
+        }
+
+        public IEnumerable<Patient.Patient> GetPatients()
+        {
+            return patients.ToList();
+        }
+
+    }
+}

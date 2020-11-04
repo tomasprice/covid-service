a9226a4901f7e654683268ee3ecce5ad177984ea
Tomasz Wodzynski <t910755@gmail.com>
2020-11-04 21:04:55 +0100

    feat: create models for service

---
+++ Patient/Patient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Patient
+{
+    [DataContract]
+    public class Patient
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Surname { get; set; }
+
+        [DataMember]
+        public int Age { get; set; }
+
+        [DataMember]
+        public DateTime FirstSymptoms { get; set; }
+
+        [DataMember]
+        public DateTime TestDate { get; set; }
+
+        [DataMember]
+        public string Address { get; set; }
+
+    }
+}
---
+++ Patient/PatientContact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patient
+{
+    [DataContract]
+    public class PatientContact
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Surname { get; set; }
+
+        [DataMember]
+        public DateTime ContactDate { get; set; }
+
+        [DataMember]
+        public string Address { get; set; }
+    }
+}
